39f49a8b217a753decb935fbea91c6cb8dbee2e0
Belov Daniil <daniil.belov@students.bfh.ch>
2017-12-15 10:47:02 +0100

    Project structure reorganized, interface for the Knapsack Item added

---
+++ Functional/KnapsackOOP/IItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KnapsackOOP
+{
+    interface IItem
+    {
+            Guid Id { get; set; }
+            string Name { get; set; }
+            int Square { get; set; }
+            int Value { get; set; }
+            int Quantity { get; set; }
+    }
+}
